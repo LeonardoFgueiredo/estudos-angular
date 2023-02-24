4d51c538b535bd5d3c6e5bdda5c046f846efbccd
Leonardo de Oliveira Figueiredo <leodbf@hotmail.com>
2023-02-24 11:19:58 -0300

    http

--- src/app/components/list-render/list-render.component.ts
+++ src/app/components/list-render/list-render.component.ts
@@ -9,14 +9,11 @@ import { ListService } from 'src/app/services/list.service';
 })
 
 export class ListRenderComponent {
-  constructor(private listService: ListService){}
+  constructor(private listService: ListService){
+    this.getAnimals();
+  }
 
-  animals: Animal[] = [
-    {name: "Tobias",  type: 'Cachorro', age: 5},
-    {name: "Lisa",    type: 'Gato',     age: 6},
-    {name: "Carlos",  type: 'Cachorro', age: 3},
-    {name: "Nina",    type: 'Gato',     age: 4},
-  ];
+  animals: Animal[] = [];
 
   animalDetails = '';
   ShowAge(animal:Animal){
@@ -24,6 +21,10 @@ export class ListRenderComponent {
   }
   removeAnimal(animal:Animal){
     this.animals = this.listService.remove(this.animals, animal);
+  }
+
+  getAnimals(): void{
+    this.listService.getAll().subscribe((animals) => (this.animals = animals));
   }
   
 }
